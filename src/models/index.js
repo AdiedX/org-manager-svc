1b3654068548e9a662e2a9a58706a580fa0206e7
Adi Sivasankaran <adi@Adis-MacBook-Pro.local>
2018-10-04 01:33:52 -0500

    Configure cloud db creds

--- src/models/index.js
+++ src/models/index.js
@@ -1,16 +1,30 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const cfenv = require('cfenv');
+const appEnv = cfenv.getAppEnv();
+const logger = require('../utils/logger');
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.cloud_db_env_variable === process.env.CLOUD_CREDS) {
+  const cloudCreds = appEnv.getService(config.cloud_db_env_variable).credentials;
+  logger.info(util.inspect(cloudCreds, { showHidden: false, depth: null }));
+  sequelize = new Sequelize(cloudCreds.cloud_sql_server, cloudCreds.username, cloudCreds.password, {
+    dialect: 'postgres'
+  });
+} else if (config.cloud_db_env_variable === 'local_config') {
+  const localCreds = require('../config/local.config.json').credentials;
+  logger.info(util.inspect(localCreds, { showHidden: false, depth: null }));
+  sequelize = new Sequelize(localCreds.cloud_sql_server, localCreds.username, localCreds.password, {
+    dialect: 'postgres'
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
